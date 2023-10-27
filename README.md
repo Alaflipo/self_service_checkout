556c7abdfd81696329d5e165e649a35af3230a5b
Morris Boers <morrisboers@gmail.com>
2023-10-27 11:33:07 +0200

    full model with some proves

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # self_service_checkout
 A mcrl2 model of a self service checkout system for the course system validation (2IMF30)
+
+## Converting files 
+How to convert mcrl2 code to a lps (linear process) file: 
+`mcrl22lps <file_name>.mcrl2 <file_name>.lps`
+
+How to convert a lps file to a lts (labeled transition system) file: 
+`lps2lts <file_name>.lps <file_name>.lts`
+
+How to view a lts file as a graph: 
+`ltsgraph <file_name>.lts`
+
+## model checking with mu calculus formulas 
+Check if a lps file holds against a mcf (model mu-calculus formula) file and convert it to a pbes (parameterised boolean equation system) file 
+
+`lps2pbes <file_name>.lps -f <file_name>.mcf <file_name>.pbes`
+
+To run the solver and check if the formula is satisfied: 
+`pbes2bool <file_name>.pbes`
--- run.py
+++ run.py
@@ -1,6 +1,24 @@
 import os
+from os import listdir
+from os.path import isfile, join
 
 # to lps 
 os.system('mcrl22lps ssc.mcrl2 out/ssc.lps')
 # to lts 
 os.system('lps2lts out/ssc.lps out/ssc.lts')
+
+# convert the lts branching bisimular to a version with internal transitions hidden
+os.system('ltsconvert -ebranching-bisim out/ssc.lts out/ssc_red.lts')
+
+# prove everything with evidence 
+path = "prove/"
+file_check = "out/ssc_red.lts"
+files = [f for f in listdir(path) if isfile(join(path, f))]
+mcf_files = [f for f in files if f.split('.')[1] == "mcf"]
+
+for file in mcf_files: 
+    name = file.split('.')[0]
+    print(f'Checking {file}:')
+    os.system(f'lts2pbes -v -c -f prove/{file} {file_check} prove/{name}.pbes')
+    os.system(f'pbessolve -v --file={file_check} --evidence-file=prove/evidence/{name}.lts prove/{name}.pbes')
+    # os.system(f'lps2lts prove/evidence/{name}.lps prove/evidence/{name}.lts')
